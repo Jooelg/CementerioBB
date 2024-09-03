59459e2ba3047fb058c99ab9be0b1781bef6c63c
jooelg <yojooel32@hotmail.es>
2024-09-03 01:23:34 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: "https://Jooelg.github.io",
+  base: "CementerioBB",
 });
